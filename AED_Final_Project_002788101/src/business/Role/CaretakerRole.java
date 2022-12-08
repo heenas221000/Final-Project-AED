920de10257340ee1c81240a425ee3aa8129c7d48
heenas221000 <114357648+heenas221000@users.noreply.github.com>
2022-12-08 15:44:10 -0500

    Commit_HS3

---
+++ AED_Final_Project_002788101/src/business/Role/CaretakerRole.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.Role;
+
+import buisness.Enterprise.Enterprise;
+import business.Network.Network;
+import buisness.Organisation.Organization;
+import business.EcoSystem;
+import javax.swing.JPanel;
+import ui.Caretaker.CaretakerJPanel;
+
+/**
+ *
+ * @author vishwa
+ */
+public class CaretakerRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise,EcoSystem business,Network network) {
+        return new CaretakerJPanel(userProcessContainer, account, organization,enterprise,business,network);
+    }
+
+    
+}
