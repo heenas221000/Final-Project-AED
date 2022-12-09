eb8b4f06e808b02d88bb700370aefc704158077d
heenas221000 <114357648+heenas221000@users.noreply.github.com>
2022-12-08 20:22:30 -0500

    conflict resolved

--- AED_Final_Project_002788101/src/business/EcoSystem.java
+++ AED_Final_Project_002788101/src/business/EcoSystem.java
@@ -1,25 +1,23 @@
-<<<<<<< Updated upstream:AED_Final_Project_002788101/src/business/EcoSystem.java
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package business;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 import business.Network.Network; 
 import business.Organization.Organization;
 import business.Role.Role; 
-=======
+
 import business.Network.Network;
 import business.Organization.Organization;
 import business.Role.Role;
->>>>>>> Stashed changes
-=======
+
 import business.Network.Network;
 import business.Organization.Organization;
 import business.Role.Role;
->>>>>>> Stashed changes
+
 import business.Role.SystemAdminRole;
 import java.util.ArrayList;
 
@@ -29,13 +27,7 @@ import java.util.ArrayList;
  */
 public class EcoSystem extends Organization {
     
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
     private static EcoSystem business;
     private ArrayList<Network> networkList;
     public static EcoSystem getInstance(){
@@ -78,79 +70,6 @@ public class EcoSystem extends Organization {
         }
         return true;
     }
-<<<<<<< Updated upstream
-    
-<<<<<<< Updated upstream
-    
-    
-    
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-package business;
-
-import business.Network.Network; 
-import business.Organization.Organization;
-import business.Role.Role; 
-import business.Role.SystemAdminRole;
-import java.util.ArrayList;
-
-/**
- *
- * @author heenashah
- */
-public class EcoSystem extends Organization{
-    
-    private static EcoSystem business;
-    private ArrayList<Network> networkList;
-    public static EcoSystem getInstance(){
-        if(business==null){
-            business=new EcoSystem();
-        }
-        return business;
-    }
-    
-    public Network createAndAddNetwork(){
-        Network network=new Network();
-        networkList.add(network);
-        return network;
-    }
-    @Override
-    public ArrayList<Role> getSupportedRole() {
-        ArrayList<Role> roleList=new ArrayList<Role>();
-        roleList.add(new SystemAdminRole());
-        return roleList;
-    }
-    private EcoSystem(){
-        super(null);
-        networkList=new ArrayList<Network>();
-    }
-
-    public ArrayList<Network> getNetworkList() {
-        return networkList;
-    }
-
-    public void setNetworkList(ArrayList<Network> networkList) {
-        this.networkList = networkList;
-    }
-    
-    public boolean checkIfUserIsUnique(String userName){
-        if(!this.getUserAccountDirectory().checkUserIsUnique(userName)){
-            return false;
-        }
-        for(Network network:networkList){
-            
-        }
-        return true;
-    }
-}
->>>>>>> Stashed changes:AED_Final_Project_002788101/src/sm/sm.java
