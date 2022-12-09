2ad9bfe2a91eb4df9e63b8b189758a2f7864e123
hetalgada15 <114257026+hetalgada15@users.noreply.github.com>
2022-12-08 20:43:40 -0500

    commit_H

---
+++ AED_Final_Project_002788101/src/business/WorkQueue/PharmacistWorkRequest.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.WorkQueue;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hetal
+ */
+public class PharmacistWorkRequest {
+      DrWorkRequest DoctorWorkRequest;
+    private ArrayList<Medicines> medicines = new ArrayList<Medicines>();
+
+    public DrWorkRequest getDoctorWorkRequest() {
+        return DoctorWorkRequest;
+    }
+
+    public void setDoctorWorkRequest(DrWorkRequest DoctorWorkRequest) {
+        this.DoctorWorkRequest = DoctorWorkRequest;
+    }
+
+    public ArrayList<Medicines> getMedicines() {
+        return medicines;
+    }
+
+    public void setMedicines(ArrayList<Medicines> medicines) {
+        this.medicines = medicines;
+    }
+    
+    
+    
+}
