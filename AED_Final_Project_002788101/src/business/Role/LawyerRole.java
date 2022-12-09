4211251cf78f5ec08110c2173f1440bce77c6ea3
hetalgada15 <114257026+hetalgada15@users.noreply.github.com>
2022-12-09 03:02:26 -0500

    commit-H
    Ui added

--- AED_Final_Project_002788101/src/business/Role/LawyerRole.java
+++ AED_Final_Project_002788101/src/business/Role/LawyerRole.java
@@ -1,7 +1,9 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+
 package business.Role;
 import business.EcoSystem;
 import business.Enterprise.Enterprise;
@@ -15,11 +17,11 @@ import ui.Lawyer.LawyerJPanel;
  *
  * @author hetal
  */
-public class LawyerRole extends Role{
-    
+public class LawyerRole extends Role {
+
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise,EcoSystem business,Network network) {
         return new LawyerJPanel(userProcessContainer, account, organization,enterprise,business,network);
     }
-    
+
 }
