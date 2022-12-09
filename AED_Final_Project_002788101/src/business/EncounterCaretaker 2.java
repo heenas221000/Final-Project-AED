6f80b70cceead7d7277bd0d4e14e60dae7aca5b7
vishwashah2001 <114256723+vishwashah2001@users.noreply.github.com>
2022-12-09 01:02:58 -0500

    lawyer-v

---
+++ AED_Final_Project_002788101/src/business/EncounterCaretaker 2.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.Encounter;
+
+/**
+ *
+ * @author hetal
+ */
+public class EncounterCaretaker {
+
+    private String encounter;
+    private String minutes;
+    private String name;
+    
+    public EncounterCaretaker(){
+    
+    }
+
+public String getEncounter() {
+        return encounter;
+    }
+
+    public void setEncounter(String encounter) {
+        this.encounter = encounter;
+    }
+
+    public String getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(String minutes) {
+        this.minutes = minutes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+
+}
